4ef08c3dc60e0a448016301061c1cc04ce2024b4
jay.pacamarra <jay.pacamarra@sanctuary.ai>
2023-12-23 15:18:11 -0800

    update: main to use config

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from config import *
 from camera import feed
 from object_detection import object_detect
 
@@ -5,7 +6,7 @@ if __name__ == "__main__":
 
     # camera index 0 - internal camera
     # camera index 2 - external camera (may need to change)
-    camera_index = 0
+    camera_index = CONFIG_cam_index
 
     # open camera
     cap = feed.start(camera_index)
@@ -15,8 +16,10 @@ if __name__ == "__main__":
 
         top_class_index = object_detect.classify_image(frame)
         is_dog = object_detect.is_dog(top_class_index)
-        text = f"{'DOG' if is_dog else '?'}"
-        feed.show(frame,text)
+
+        if CONFIG_show_feed:
+            text = f"{'DOG' if is_dog else '?'}"
+            feed.show(frame,text)
 
         # Break the loop if the user presses the 'q' key
         if feed.is_quit_key_pressed():
