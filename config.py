4e396b92e7758cd02ace844ebbcaca459d09163b
jay.pacamarra <jay.pacamarra@sanctuary.ai>
2023-12-23 15:55:13 -0800

    add: timing of dog potty detection
    - update CONFIG_dog_potty_timeout_time_seconds to 2

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 CONFIG_cam_index = 0 #0-interior cam, 2-external cam
 CONFIG_show_feed = 1 #1-true, 0-false
-CONFIG_dog_potty_timeout_time_seconds = 4 #time in seconds before alert is generated
+CONFIG_dog_potty_timeout_time_seconds = 2 #time in seconds before alert is generated
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from config import *
 from camera import feed
 from object_detection import object_detect
+import time
+
 
 if __name__ == "__main__":
 
@@ -11,11 +13,25 @@ if __name__ == "__main__":
     # open camera
     cap = feed.start(camera_index)
 
+    # flag to see if timer is started
+    f_timer_started = False
+
     while True:
         frame = feed.get(cap)
 
         top_class_index = object_detect.classify_image(frame)
         is_dog = object_detect.is_dog(top_class_index)
+
+        if is_dog:
+            if not f_timer_started:
+                start_time = time.time()
+                f_timer_started = True
+            elapsed_time = int(time.time() - start_time)
+            if elapsed_time > CONFIG_dog_potty_timeout_time_seconds:
+                #generate alert
+                print("Dog Potty Alert")
+        else:
+            f_timer_started = False
 
         if CONFIG_show_feed:
             text = f"{'DOG' if is_dog else '?'}"
