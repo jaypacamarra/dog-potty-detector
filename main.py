79b57e1f7c70651a2c7f6b940df92a26a5fe67b0
jay.pacamarra <jay.pacamarra@sanctuary.ai>
2024-02-27 19:21:05 -0800

    update: use rpi gpio input for detecting doggos

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
-from config import *
-from camera import feed
-from object_detection import object_detect
+import os
 import time
+import RPi.GPIO as GPIO
+from pushbullet import Pushbullet
 
+TIMEOUT = 10
 
 if __name__ == "__main__":
+    pb = Pushbullet(os.environ["PUSHBULLET_API_KEY"])
 
-    # camera index 0 - internal camera
-    # camera index 2 - external camera (may need to change)
-    camera_index = CONFIG_cam_index
+    PIR = 11
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(PIR, GPIO.IN)
 
-    # open camera
-    cap = feed.start(camera_index)
-
-    # flag to see if timer is started
-    f_timer_started = False
+    timeout_sec = TIMEOUT
+    notified = False
+    wait = 5
 
     while True:
-        frame = feed.get(cap)
+        if GPIO.input(PIR):
+            time.sleep(wait)
+            timeout_sec = timeout_sec - wait
+            if timeout_sec:
+                if GPIO.input(PIR):
+                    if not notified:
+                        pb.push_note("Doggo Detected", "Doggo needs to potty")
+                        notified = True
+            else:
+                # reset flags after timeout_sec so we can send notifications again
+                notified = False
+                timeout_sec = TIMEOUT
 
-        top_class_index = object_detect.classify_image(frame)
-        is_dog = object_detect.is_dog(top_class_index)
 
-        if is_dog:
-            if not f_timer_started:
-                start_time = time.time()
-                f_timer_started = True
-            elapsed_time = int(time.time() - start_time)
-            if elapsed_time > CONFIG_dog_potty_timeout_time_seconds:
-                #generate alert
-                print("Dog Potty Alert")
-        else:
-            f_timer_started = False
-
-        if CONFIG_show_feed:
-            text = f"{'DOG' if is_dog else '?'}"
-            feed.show(frame,text)
-
-        # Break the loop if the user presses the 'q' key
-        if feed.is_quit_key_pressed():
-            break
-    # cleanup
-    feed.exit(cap)
